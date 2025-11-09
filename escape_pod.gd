cf810714f7a983dc5ac0297cddced2e9cc8ddccd
augustgrantham <august.grantham@gmail.com>
2025-11-09 10:17:15 -0600

    FINAL FOR REAL

--- escape_pod.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-func _on_exit_area_body_entered(body: Node2D) -> void:
-	var winCondition = global.canLeave
-	if(body.name == "player"):
-		if(winCondition):
-			print("You win!")
